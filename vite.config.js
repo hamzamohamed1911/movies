599f304ec5af27c1e3b347c4d85daf56857f9cd7
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-05-18 13:51:22 +0300

    vite confiq

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Add the rewrites section here
+  rewrites: async () => [
+    { source: /^\/(?!.*\.hot|favicon\.ico).*/, destination: '/index.html' },
+  ],
+});
