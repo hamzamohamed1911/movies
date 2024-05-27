ff8a749b37309c2b46a8e52d09d6ad3ad09bc854
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-05-27 13:46:56 +0300

    new pt-6

--- src/_root/components/Details.jsx
+++ src/_root/components/Details.jsx
@@ -27,7 +27,7 @@ const Details = ({ item }) => {
         alt={item.title}
         className="w-full lg:h-[600px] h-[550px] object-cover rounded-tl-[50px] p-4"
       />
-      <div className="absolute inset-0  lg:p-24 bg-gradient-to-b from-transparent to-slate-950  flex flex-col justify-center items-center sm:items-start text-white rounded-lg shadow-lg">
+      <div className="absolute inset-0 pt-6 lg:p-24 bg-gradient-to-b from-transparent to-slate-950  flex flex-col justify-center items-center sm:items-start text-white rounded-lg shadow-lg">
         <div className="flex flex-col items-center sm:flex-row space-y-6 sm:space-y-0 sm:space-x-20">
           <img
             src={item.posterUrl}
