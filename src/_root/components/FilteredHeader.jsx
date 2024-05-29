b1be4d017d1da04fa48ddf1f50b65ec2fb56e56f
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-05-29 21:23:54 +0300

    filtring

--- src/_root/components/FilteredHeader.jsx
+++ src/_root/components/FilteredHeader.jsx
@@ -1,26 +1,38 @@
 import React, { useState } from 'react';
 
-const FilteredHeader = ({ label }) => {
+const FilteredHeader = ({ label, onYearChange ,onRatingChange}) => {
   const [selectedYear, setSelectedYear] = useState('');
   const [selectedLanguage, setSelectedLanguage] = useState('');
   const [selectedRating, setSelectedRating] = useState('');
   const [selectedKind, setSelectedKind] = useState('');
 
-  const handleYearChange = (event) => {
-    setSelectedYear(event.target.value);
-  };
+
 
   const handleLanguageChange = (event) => {
     setSelectedLanguage(event.target.value);
   };
 
   const handleRatingChange = (event) => {
-    setSelectedRating(event.target.value);
+    const rate = event.target.value;
+
+    setSelectedRating(rate);
+    onRatingChange(rate)
   };
 
   const handleKindChange = (event) => {
     setSelectedKind(event.target.value);
   };
+
+
+  const handleYearChange = (event) => {
+    const year = event.target.value;
+    setSelectedYear(year);
+    onYearChange(year); 
+  };
+  const currentYear = new Date().getFullYear();
+  const years = Array.from({ length: 20 }, (_, index) => currentYear - index);
+
+
 
   return (
     <div className="bg-transparent text-white p-6 rounded-lg">
@@ -28,23 +40,21 @@ const FilteredHeader = ({ label }) => {
         <h1 className="lg:text-6xl text-3xl font-bold text-babyblue mb-4 lg:mb-0">{label}</h1>
         <div className="flex flex-wrap justify-center lg:justify-end space-y-4 lg:space-y-0 lg:space-x-4 text-xl">
 
-          <select className='bg-navy p-3 rounded-3xl mb-4 lg:mb-0 w-72 lg:w-auto' value={selectedYear} onChange={handleYearChange}>
-            <option value="">Filter by Year</option>
-            <option value="2024">2024</option>
-            <option value="2023">2023</option>
-            <option value="2022">2022</option>
-            <option value="2021">2021</option>
-            <option value="2020">2020</option>
-          </select>
+        <select className='bg-navy p-3 rounded-3xl mb-4 lg:mb-0 w-72 lg:w-auto' value={selectedYear} onChange={handleYearChange}>
+  <option value=""> Year</option>
+  {years.map((year) => (
+    <option key={year} value={year}>{year}</option>
+  ))}
+</select>
           <select className='bg-navy p-3 rounded-3xl mb-4 lg:mb-0 w-72 lg:w-auto' value={selectedLanguage} onChange={handleLanguageChange}>
-            <option value="">Filter by Language</option>
+            <option value=""> Language</option>
             <option value="Arabic">Arabic</option>
             <option value="English">English</option>
             <option value="Spanish">Spanish</option>
             <option value="French">French</option>
           </select>
           <select className='bg-navy p-3 rounded-3xl mb-4 lg:mb-0 w-72 lg:w-auto' value={selectedRating} onChange={handleRatingChange}>
-            <option value="">Filter by Rating</option>
+            <option value=""> Rating</option>
             <option value="9">9</option>
             <option value="8">8</option>
             <option value="7">7</option>
@@ -53,7 +63,7 @@ const FilteredHeader = ({ label }) => {
             <option value="4">4</option>
           </select>
           <select className='bg-navy p-3 rounded-3xl w-72 lg:w-auto' value={selectedKind} onChange={handleKindChange}>
-            <option value="">Filter by Kind</option>
+            <option value=""> Kind</option>
             <option value="Action">Action</option>
             <option value="Romantic">Romantic</option>
             <option value="Comedy">Comedy</option>
--- src/_root/pages/Tv.jsx
+++ src/_root/pages/Tv.jsx
@@ -1,6 +1,6 @@
-import React, { memo, useMemo, useState } from 'react';
-import { AiFillStar } from 'react-icons/ai'; 
+import React, { useState, useMemo } from 'react';
 import { dummyData } from '../../constants';
+import { AiFillStar } from 'react-icons/ai'; 
 import { Link } from 'react-router-dom';
 import FilteredHeader from '../components/FilteredHeader';
 import Pagination from '../components/Pagination';
@@ -9,75 +9,96 @@ import SliderComponent from '../components/SliderComponent';
 const Tv = () => {
   const limit = 20;
   const [currentPage, setCurrentPage] = useState(1);
+  const [selectedYear, setSelectedYear] = useState('');
+  const [selectedRating, setSelectedRating] = useState(''); // State variable for selected rating
   const lastIndex = currentPage * limit;
   const firstIndex = lastIndex - limit;
+
   const tvShows = dummyData.filter(show => show.type === 'tv');
-  const records = useMemo(() => [...tvShows.slice(firstIndex, lastIndex)], [firstIndex, lastIndex, tvShows]);
-  const nPage = Math.ceil(tvShows.length / limit);
-  const numbers = [...Array(nPage + 1).keys()].slice(1);
+  const filteredTvShows = useMemo(() => {
+    let filteredData = tvShows;
+
+    if (selectedYear) {
+      filteredData = filteredData.filter(show => show.year === parseInt(selectedYear));
+    }
+
+    if (selectedRating) {
+ const selectedFloatRating = parseFloat(selectedRating);
+
+  filteredData = filteredData.filter(show => show.rating === selectedFloatRating);
+  }
+
+    return filteredData;
+  }, [selectedYear, selectedRating, tvShows]);
   
+  const records = filteredTvShows.slice(firstIndex, lastIndex);
+  const nPage = Math.ceil(filteredTvShows.length / limit);
+  const numbers = [...Array(nPage + 1).keys()].slice(1);
+
+  const handleYearChange = (year) => {
+    setSelectedYear(year);
+  };
+
+  const handleRatingChange = (rating) => {
+    setSelectedRating(rating);
+    setCurrentPage(1);
+  };
+
   return (
-   <>
-    <section  className='relative justify-center flex items-center'>
-      <div className=' lg:p-20 pt-10 p-5 '>
-    <div className="flex flex-col">
-          <div className=" grid grid-cols-1 ...">
-          <SliderComponent data={tvShows} />
+    <>
+      <section className='relative justify-center flex items-center'>
+        <div className='lg:p-20 pt-10 p-5 '>
+          <div className="flex flex-col">
+            <div className="grid grid-cols-1 ...">
+              <SliderComponent data={tvShows} />
+            </div>
           </div>
-     </div>
 
-     <div className=' pb-8 space-y-10'>
-      <FilteredHeader label="All Tv" />
-      
-      <Pagination nPage={nPage} setcurrentPage={setCurrentPage} currentPage={currentPage} numbers={numbers}/>
+          <div className='pb-8 space-y-10'>
+            <FilteredHeader 
+              label="All Tv" 
+              onYearChange={handleYearChange} 
+              onRatingChange={handleRatingChange} // Pass handleRatingChange to FilteredHeader
+            />
 
-</div>
-      <div className="flex flex-col">
+            <Pagination nPage={nPage} setcurrentPage={setCurrentPage} currentPage={currentPage} numbers={numbers} />
+          </div>
 
-        <div className="flex-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10 ">
-          {records.map(show => (
-            <TvShowItem key={show.id} show={show} />
-          ))}
-
+          <div className="flex flex-col">
+            <div className="flex-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10 ">
+              {records.map(show => (
+                <TvShowItem key={show.id} show={show} />
+              ))}
+            </div>
+            <div className='pt-8'>
+              <Pagination nPage={nPage} setcurrentPage={setCurrentPage} currentPage={currentPage} numbers={numbers} />
+            </div>
+          </div>
         </div>
-<div className='pt-8'>
-<Pagination nPage={nPage} setcurrentPage={setCurrentPage} currentPage={currentPage} numbers={numbers}/>
-
-</div>
-      </div>
-      </div>
-    </section>
-   </>
+      </section>
+    </>
   );
 };
 
-const TvShowItem =memo( ({ show }) => {
+const TvShowItem = ({ show }) => {
   return (
-
-<div className="justify-center flex items-center">
+    <div className="justify-center flex items-center">
       <div>
-      <div className="h-[440px] w-[300px] relative ">
-      <Link to={`/tv/${show.id}`}>
-        <img src={show.posterUrl} alt={show.title} className="object-cover h-full w-full rounded-lg  " />
-        </Link>
-      </div>
-      <div className="pt-4 text-babyblue h-24 overflow-hidden">
-      <div className="flex items-center gap-2">
-           <span className="lg:text-2xl text-xl">{show.rating}</span>
-           <AiFillStar
-                    className='h-7 w-7 fill-yellow-400'               
-                    
-                  />
+        <div className="h-[440px] w-[300px] relative ">
+          <Link to={`/tv/${show.id}`}>
+            <img src={show.posterUrl} alt={show.title} className="object-cover h-full w-full rounded-lg  " />
+          </Link>
+        </div>
+        <div className="pt-4 text-babyblue h-24 overflow-hidden">
+          <div className="flex items-center gap-2">
+            <span className="lg:text-2xl text-xl">{show.rating}</span>
+            <AiFillStar className='h-7 w-7 fill-yellow-400' />
           </div>
           <h2 className="lg:text-2xl md:text-2xl text-xl  font-bold mb-2 max-w-[300px]  ">{show.title}</h2>
-         
-
+        </div>
       </div>
-      </div>
- </div>
-
-
-);
-});
+    </div>
+  );
+};
 
 export default Tv;
