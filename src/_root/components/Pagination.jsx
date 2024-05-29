eb1ff23377c968b46232fccd4991f67673393e19
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-05-29 18:19:52 +0300

    pagination buttons

--- src/_root/components/Pagination.jsx
+++ src/_root/components/Pagination.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { IoIosArrowForward  , IoIosArrowBack} from "react-icons/io";
-
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 
 const Pagination = ({ nPage, setcurrentPage, numbers, currentPage }) => {
 
@@ -19,34 +18,45 @@ const Pagination = ({ nPage, setcurrentPage, numbers, currentPage }) => {
   const changePage = (id) => {
     setcurrentPage(id);
   };
-  console.log('paginaytion')
 
   return (
-    <ol className="flex justify-center gap-1  font-lg">
+    <ol className="flex justify-center gap-1 font-lg">
       <li>
         <a
           href="#"
-          className="inline-flex size-10  items-center justify-center rounded border border-gray-300 bg-navy text-white rtl:rotate-180"
+          className={`inline-flex size-10 items-center justify-center rounded border border-gray-300 text-white rtl:rotate-180 ${
+            currentPage === 1 ? 'bg-gray-400 cursor-default ' : 'bg-navy cursor-pointer'
+          }`}
         >
-          <button onClick={PrevPage}>
-            <IoIosArrowBack className='h-7 w-7 ' />
+          <button className={`${currentPage === 1 ?  'cursor-default' : 'cursor-pointer'}`} onClick={PrevPage}>
+            <IoIosArrowBack className="h-7 w-7" />
           </button>
         </a>
       </li>
 
       {numbers.map((n, i) => (
-        <li onClick={() => changePage(n)} key={i} className={` size-10  rounded border border-gray-300 text-center flex justify-center items-center text-white cursor-pointer ${currentPage === n ? 'bg-blue text-white active' : 'bg-navy hover:bg-blue'}`}>
-          <a className='text-xl' href="#">{n}</a>
+        <li
+          onClick={() => changePage(n)}
+          key={i}
+          className={`size-10 rounded border border-gray-300 text-center flex justify-center items-center text-white cursor-pointer ${
+            currentPage === n ? 'bg-blue text-white active' : 'bg-navy hover:bg-blue'
+          }`}
+        >
+          <a className="text-xl" href="#">
+            {n}
+          </a>
         </li>
       ))}
 
       <li>
-        <a
+      <a
           href="#"
-          className="inline-flex size-10 items-center justify-center rounded border border-gray-300 bg-navy text-white rtl:rotate-180"
+          className={`inline-flex size-10 items-center justify-center rounded border border-gray-300 ${
+            currentPage === nPage ? 'bg-gray-400 cursor-default' : 'bg-navy cursor-pointer'
+          } text-white rtl:rotate-180`}
         >
-          <button onClick={NextPage}>
-            <IoIosArrowForward className='h-7 w-7 ' />
+          <button className={`${currentPage === nPage ? 'cursor-default' : 'cursor-pointer'} }`} onClick={NextPage}>
+            <IoIosArrowForward className="h-7 w-7" />
           </button>
         </a>
       </li>
