69ef6dd6e800d9dab78d364dad64d4d7a8976f1f
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-06-04 02:31:01 +0300

    new

--- src/_root/components/MoviesAndTVShows.jsx
+++ src/_root/components/MoviesAndTVShows.jsx
@@ -23,22 +23,21 @@ const MoviesAndTVShows = ({ personId }) => {
   return (
     <div className=" text-babyblue p-8 bg-gray-900">
  <h1 className='lg:text-7xl md:text-4xl text-3xl font-semibold text-blue'> Known For</h1>
-
-      <div className=" lg:text-3xl text-2xl py-10">
-        <button
-          className={`mr-4 focus:outline-none ${mediaType === 'movie' ? 'text-blue-400 border-[1px] rounded-xl  p-3 border-blue' : 'text-gray-400'}`}
-          onClick={() => handleToggle('movie')}
-        >
-          Movies
-        </button>
-        <button
-          className={`focus:outline-none ${mediaType === 'tv' ? 'text-blue-400 border-[1px] rounded-xl  p-3 border-blue' : 'text-gray-400'}`}
-          onClick={() => handleToggle('tv')}
-        >
-          TV Shows
-        </button>
-      </div>
-      
+ 
+ <div className="lg:text-3xl text-2xl py-10">
+  <button
+    className={`mr-4 focus:outline-none hover:text-blue transition duration-500 ease-in-out ${mediaType === 'movie' ? 'text-blue border-[1px] rounded-xl p-3 border-blue' : 'text-gray-400'}`}
+    onClick={() => handleToggle('movie')}
+  >
+    Movies
+  </button>
+  <button
+    className={`focus:outline-none transition duration-500 ease-in-out ${mediaType === 'tv' ? 'text-blue border-[1px] rounded-xl p-3 border-blue' : 'text-gray-400'}`}
+    onClick={() => handleToggle('tv')}
+  >
+    TV Shows
+  </button>
+</div>
      
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
           {filteredMediaList.map((media , index) => (
