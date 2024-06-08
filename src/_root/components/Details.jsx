04da5dfa0ea143a65bd7b614f1c5db6a8ebd2611
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-06-08 09:13:40 +0300

    respove details

--- src/_root/components/Details.jsx
+++ src/_root/components/Details.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { AiFillStar } from "react-icons/ai";
+import { AiFillStar } from 'react-icons/ai';
 import Button from '../components/Button';
-import { IoMdAdd } from "react-icons/io";
-import { FaPlay } from "react-icons/fa";
+import { IoMdAdd } from 'react-icons/io';
+import { FaPlay } from 'react-icons/fa';
 import { useComponentContext } from '../../store/componentContext';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import { useNavigate } from 'react-router-dom';
@@ -11,7 +11,6 @@ const Details = ({ item }) => {
   const { addToWatchlist } = useComponentContext();
   const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
-
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -27,18 +26,15 @@ const Details = ({ item }) => {
 
   if (isLoading) {
     return (
-      <SkeletonTheme baseColor="#1B262C" highlightColor="#1B263A ">
-
-      <div className="container mx-auto px-4 py-10">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-          <div className="col-span-1 sm:col-span-2">
-            <Skeleton height={550} />
+      <SkeletonTheme baseColor="#1B262C" highlightColor="#1B263A">
+        <div className="container mx-auto px-4 py-10">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+            <div className="col-span-1 sm:col-span-2">
+              <Skeleton height={550} />
+            </div>
           </div>
-          
         </div>
-      </div>
       </SkeletonTheme>
-
     );
   }
 
@@ -47,17 +43,17 @@ const Details = ({ item }) => {
       <img
         src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`}
         alt={item.title}
-        className="w-full lg:h-[600px] h-[550px] object-cover rounded-tl-[50px] p-4"
+        className="w-full lg:h-[600px] h-[400px] object-cover rounded-tl-[50px] p-4"
       />
       <div className="absolute inset-0 p-4 lg:p-24 bg-gradient-to-b from-transparent to-slate-950 flex flex-col justify-center items-center sm:items-start text-white rounded-lg shadow-lg">
-        <div className="flex flex-col justify-center items-center sm:flex-row lg:space-y-6 space-y-4 sm:space-x-10">
+        <div className="flex flex-col sm:flex-row items-center sm:items-start lg:space-y-6 space-y-4 sm:space-x-10">
           <img
             src={`https://image.tmdb.org/t/p/w400${item.poster_path}`}
             alt={item.title}
-            className="w-52 h-72 lg:w-[320px] pt-6 lg:h-[460px] object-fill rounded-lg shadow-md"
+            className="w-52 h-72 lg:w-[320px] lg:h-[460px] object-fill rounded-lg shadow-md"
           />
-          <div className="text-center sm:text-left">
-            <h2 className="lg:text-3xl text-2xl font-semibold mb-4">{item.title ||item.name}</h2>
+          <div className="text-center sm:text-left max-w-full sm:max-w-md lg:max-w-lg">
+            <h2 className="lg:text-3xl text-2xl font-semibold mb-4">{item.title || item.name}</h2>
             <div className="flex justify-center sm:justify-start mb-4 text-lg">
               {[...Array(5)].map((_, i) => (
                 <AiFillStar
@@ -69,8 +65,8 @@ const Details = ({ item }) => {
               ))}
               <span className="ml-3 text-2xl">{item.vote_average} / 10</span>
             </div>
-            <div className="flex space-x-4 pt-4 justify-center sm:justify-start">
-              <Button  handleClick={()=> navigate(`/movie/trailer/${item.id}`)}  normal icon={<FaPlay />} label="Play Trailer" />
+            <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 pt-4 justify-center sm:justify-start">
+              <Button handleClick={() => navigate(`/movie/trailer/${item.id}`)} normal icon={<FaPlay />} label="Play Trailer" />
               <Button icon={<IoMdAdd />} backgroundColor normal label="Add to Watchlist" handleClick={handleAddToWatchlist} />
             </div>
           </div>
