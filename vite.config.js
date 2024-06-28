581c342c16850160d5acc7f16910bb5148ad7c6f
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-06-28 20:57:08 +0300

    new

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import MillionLint from "@million/lint";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [MillionLint.vite()],
+  plugins: [
+    MillionLint.vite(),
+    // Other plugins if any
+  ],
   server: {
-    host: true,
-    port: 5173,
-  }
+    host: true, // Use the default host
+    port: 5173, // Specify the desired port
+  },
 });
